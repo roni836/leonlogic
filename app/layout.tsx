820b7e0941e6fcd787a5b26a96107f384a033431
roni836 <ronitsaha836@gmail.com>
2025-08-21 17:05:35 +0530

    new meta code

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,32 +66,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     }}
                 />
                 <meta name="robots" content="index, follow" />
-                {/* Meta Pixel */}
-                <Script id="fb-pixel-script" strategy="afterInteractive">
-                    {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1745661109653565');
-            fbq('track', 'PageView');
-            fbq('track', 'Lead');
-          `}
-                </Script>
-                <noscript
-                    dangerouslySetInnerHTML={{
-                        __html: `
-              <img height="1" width="1" style="display:none"
-                   src="https://www.facebook.com/tr?id=1745661109653565&ev=PageView&noscript=1"/>
-            `,
-                    }}
-                />
+                {/* Meta Pixel (loads after hydration) */}
+        <Script id="fb-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1745661109653565');
+          fbq('track', 'PageView');
+          fbq('track', 'Lead');
+        `}</Script>
             </head>
             <body>
+                 <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src={`https://www.facebook.com/tr?id=1745661109653565&ev=PageView&noscript=1`}
+            alt=""
+          />
+        </noscript>
                 <Providers>
                     <App />
                     <ConditionalLayout>
