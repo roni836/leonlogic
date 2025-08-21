9840ccbd778ffb2cdafe88d8f67ad53151baba4e
roni836 <ronitsaha836@gmail.com>
2025-08-21 16:29:59 +0530

    facebook ads things added

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,9 @@ import type { Metadata } from 'next';
 import helper from '@/libs/helper';
 import { Space_Grotesk } from 'next/font/google';
 import ConditionalLayout from '@/components/ConditionalLayout';
+import Script from 'next/script';
+
+const FB_PIXEL_ID = '1745661109653565';
 
 export const metadata: Metadata = {
     metadataBase: new URL(process.env.NEXT_PUBLIC_APP_URL || 'http://localhost:3000'),
@@ -65,6 +68,29 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
                     }}
                 />
                 <meta name="robots" content="index, follow" />
+                {/* Meta Pixel */}
+                <Script id="fb-pixel-script" strategy="afterInteractive">
+                    {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `}
+                </Script>
+                <noscript
+                    dangerouslySetInnerHTML={{
+                        __html: `
+              <img height="1" width="1" style="display:none"
+                   src="https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1"/>
+            `,
+                    }}
+                />
             </head>
             <body>
                 <Providers>
